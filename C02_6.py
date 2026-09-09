669c5921d5ec5052b2a8a6e4eb4dc3ac2758da14
Aman Remesh <amanremesh@gmail.com>
2026-09-09 12:25:37 +0530

    Add files via upload

---
+++ C02_6.py
@@ -0,0 +1,10 @@
+s = input("Enter a string: ")
+freq = {}
+for char in s:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+print("Character frequency:")
+for char, count in freq.items():
+    print(char, ":", count) 
